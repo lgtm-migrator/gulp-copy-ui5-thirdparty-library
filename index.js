161db12075f64c65f29a1a48672cca236cf7c561
Theo Sun <theo.sun@sap.com>
2018-10-30 13:15:18 +0800

    feat(output): indet-lib page

--- index.js
+++ index.js
@@ -3,7 +3,44 @@ var File = require('vinyl');
 var rollup = require("rollup");
 var rollupNodeResolve = require("rollup-plugin-node-resolve");
 var rollupCjs = require("rollup-plugin-commonjs");
-var uglify = require("rollup-plugin-uglify")
+var { uglify } = require("rollup-plugin-uglify");
+
+var indexHtmlTemplate = (
+  namespace,
+  resourceHost = "openui5.hana.ondemand.com",
+  packages = [],
+  indexPageTitle = "UI5 Project",
+) => {
+  var packagesObject = packages.reduce((p, c) => { p[c] = c; return p; }, {});
+  var resouceroots = Object.assign(packagesObject, { [namespace]: "." });
+  var resoucerootsJson = JSON.stringify(resouceroots);
+  return `<!DOCTYPE html>
+  <html>
+  
+  <head>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta charset="utf-8">
+      <title>
+          ${indexPageTitle}
+      </title>
+      <script 
+          id="sap-ui-bootstrap" 
+          src="https://${resourceHost}/resources/sap-ui-core.js" 
+          data-sap-ui-theme="sap_belize"
+          data-sap-ui-libs="sap.m" 
+          data-sap-ui-compatVersion="edge" 
+          data-sap-ui-resourceroots='${resoucerootsJson}'
+      >
+      </script>
+  </head>
+  
+  <body class="sapUiBody" id="content">
+      <script src="./index.js"></script>
+  </body>
+  
+  </html>
+  `;
+};
 
 var formatUI5Module = (umdCode, mName) => `sap.ui.define(function(){
   ${umdCode}
@@ -13,12 +50,14 @@ var formatUI5Module = (umdCode, mName) => `sap.ui.define(function(){
 
 var rollupTmpConfig = (mAsbPath, mName) => ({
   input: mAsbPath,
-  preferBuiltins: false,
   output: {
     file: `${mName}.js`,
     format: 'umd'
   },
-  plugins: [rollupNodeResolve(), rollupCjs(), uglify()]
+  onwarn: function (message) {
+    // do nothing
+  },
+  plugins: [rollupNodeResolve({ preferBuiltins: true, }), rollupCjs(), uglify()]
 });
 
 const resolve = (mName) => {
@@ -32,14 +71,24 @@ const bundleModule = async (mName) => {
   return formatUI5Module(generated.code, mName);
 };
 
-module.exports = function () {
+module.exports = function (
+  { namespace,
+    resouceHost = "openui5.hana.ondemand.com",
+    indexPageTitle = "UI5 Project",
+    indexPageTarget = "index-with-lib.html"
+  }
+) {
 
-  return through2.obj(function (file, encoding, cb) {
-    try {
-      var packageJson = JSON.parse(file.contents.toString());
-      var deps = packageJson.dependencies;
-      if (deps) {
-        Promise
+  if (!namespace) {
+    throw new Error("You must set namespace for gulp-copy-ui5-thirdparty-library !");
+  }
+
+  return through2.obj(async function (file, encoding, cb) {
+    var packageJson = JSON.parse(file.contents.toString());
+    var deps = packageJson.dependencies;
+    if (deps) {
+      try {
+        await Promise
           .all(
             Object.keys(deps).map(async d => {
               const code = await bundleModule(d);
@@ -48,18 +97,24 @@ module.exports = function () {
                 contents: Buffer.from(code)
               }));
             })
-          )
-          .then(() => {
-            cb();
-          })
-          .catch(err => {
-            cb(err);
-          });
-      } else {
-        cb();
+          );
+      } catch (error) {
+        cb(error);
       }
-    } catch (error) {
-      cb(error);
     }
+    var indexHtml = indexHtmlTemplate(
+      namespace,
+      resouceHost,
+      deps ? Object.keys(deps) : [],
+      indexPageTitle
+    );
+
+    this.push(new File({
+      path: indexPageTarget,
+      contents: Buffer.from(indexHtml)
+    }));
+
+    cb();
+
   });
 };
